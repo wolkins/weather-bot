9e6fd4244544ee6bfd1ad18005d38d71f98c2f64
wolkins <h.fukuhara0606@gmail.com>
2017-04-08 10:47:02 +0900

    test

--- weather-bot/src/main/scala/example/Hello.scala
+++ weather-bot/src/main/scala/example/Hello.scala
@@ -2,13 +2,17 @@ package example
 import skinny.http._
 import org.json4s._
 import org.json4s.native.JsonMethods._
-
+import org.json4s.JsonDSL.WithDouble._
 
 object Hello extends Greeting with App {
   val res = HTTP.get("http://weather.livedoor.com/forecast/webservice/json/v1?city=130010")
-  println(parse(res.textBody))
+  val desc: List[String] = for {
+      JObject(description) <- parse(res.textBody)
+      JField("text", JString(text)) <- description
+    } yield text
+  val json =  compact(render(("text" -> desc.head)))
+  HTTP.post("https://hooks.slack.com/services/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", json)
+
 }
 
-trait Greeting {
-  lazy val greeting: String = "Hello  "
-}
+trait Greeting {}
